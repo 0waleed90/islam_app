a55f89076e3ae2a02d8443ca05aa39cd0c268a7c
0waleed90 <nmrghsi@gmail.com>
2026-04-07 17:09:32 +0300

    build Splash View Ui done

--- lib/features/spash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/spash/presentation/view/widgets/splash_view_body.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack(children: [
-      Image.asset('assets/photos/splash_background.png',fit: BoxFit.fill,)
-      
-      
-      
-    ],),);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset('assets/photos/splash_background.png', fit: BoxFit.fill),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: .end,
+                  crossAxisAlignment: .start,
+
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: .end,
+
+                        children: [
+                          SafeArea(
+                            bottom: false,
+
+                            child: Image.asset('assets/photos/Mosque-01 1.png'),
+                          ),
+
+                          Align(
+                            alignment: .topLeft,
+                            child: Image.asset('assets/photos/Shape-07 1.png'),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Image.asset(
+                      'assets/photos/Glow.png',
+                      width: 70,
+                      height: 300,
+                    ),
+                  ],
+                ),
+                Image.asset('assets/photos/OBJECTS.png'),
+                Image.asset('assets/photos/Islami.png'),
+                // SizedBox(height: 50,),
+                // Text("Programming py waleed Maksour",style: TextStyle(fontWeight: .w900,fontSize: 20,color: Colors.white10),),
+                Align(
+                    alignment: .centerRight,
+                    child: Image.asset('assets/photos/Shape-04 1.png')),
+
+                 Text("Programming py waleed Al-Maksour",style: TextStyle(fontWeight: .w900,fontSize: 20,color: Colors.white10),),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+//Image.asset('assets/photos/Glow.png', width: 80),
+// Image.asset('assets/photos/Shape-07 1.png'),
