702f688a349be30a98766ad2e92460332c75e05a
0waleed90 <nmrghsi@gmail.com>
2026-04-08 16:14:05 +0300

    create Intro view and navigate to it

---
+++ lib/core/util/styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:islam/core/constants.dart';
+
+class Styles {
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: .bold,
+    color: kPrimaryColor,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: .bold,
+    color: kPrimaryColor,
+  );
+}
--- lib/features/intro/presentation/view/widgets/intro_view_body.dart
+++ lib/features/intro/presentation/view/widgets/intro_view_body.dart
@@ -1,18 +1,32 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:islam/features/intro/presentation/view/widgets/custom_Intro_view.dart';
 class IntroViewBody extends StatelessWidget {
   const IntroViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: CustomIntroView(),);
   }
 }
-class CustomIndicator extends StatelessWidget {
-  const CustomIndicator({super.key});
+class CustomDotsIndicator extends StatelessWidget {
+  const CustomDotsIndicator({super.key,required this.dotsIndex});
+ final double dotsIndex;
+  @override
+  Widget build(BuildContext context) {
+    return DotsIndicator(
+      position:dotsIndex ,
+      dotsCount: 4,decorator: DotsDecorator(color: Colors.yellow),);
+  }
+}
+class CustomPageView extends StatelessWidget {
+  const CustomPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DotsIndicator(dotsCount: 4);
+    return PageView(children: [
+
+
+    ],);
   }
 }
