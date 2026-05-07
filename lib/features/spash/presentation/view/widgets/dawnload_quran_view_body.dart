17509dbdd2f7de55f7d160af3d9bf6b46b8a833c
0waleed90 <nmrghsi@gmail.com>
2026-05-07 19:31:36 +0300

    create dawnload_quran_view

---
+++ lib/features/spash/presentation/view/widgets/dawnload_quran_view_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DawnloadQuranViewBody extends StatelessWidget {
+  const DawnloadQuranViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator();
+  }
+}
